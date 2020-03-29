cd623ab8843fb8c9f2f3418bcd182b740c6ab23d
Martin Davis <martin_davis3@yahoo.ca>
2020-03-29 12:18:42 -0600

    add all initial code

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
 // My functions
 
-const isDigit = ( char ) => {
-    let cc = char.charCodeAt(0);
+const isDigit = ( testChar ) => {
+    let cc = testChar.charCodeAt(0);
     return cc > 47 && cc < 58; 
 };
 
-const isUppercase = ( char ) => { return nextChar === nextChar.toUpperCase() };
+const isUppercase = ( testChar ) => { return testChar === testChar.toUpperCase() };
 
+const mergeFormulae = ( formulaA, formulaB ) => {
+    //Combines two formulae, held in seperate maps, into a new formula in the returned map.
+    for (let [key, value] of formulaB.entries()) {
+        //The chem element is already in formulaA, so add new number of atoms to original number.
+        if ( formulaA.has( key ) ) {
+            let ValA = formulaA.value( key );
+            ValA += value;
+            formulaA.set( key, valA);
+        } else {
+            //The chem element is not in formula, so add new chem element with number of atoms.
+            formulaA.set( key, value );
+        }
+    }
+    return formulaA;
+}
 
-
-var inputForm = document.getElementById( 'input-form' );
-
-// Form submission listener
-inputForm.addEventListener( 'submit', function ( event ) {
-    event.preventDefault();
-    // Get the input
-    var chemFormula = document.getElementById( 'chem-formula-input' ).value;
-} );
-
-molFormula = new Map();
-
-molFormula = function( chemFormula ) {
+makeMolFormula = function( chemFormula ) {
     var currChar, nextChar, currChemSymb, currNumString;
     //Parsing algorithm uses next character to decide what to do with previous one,
     //so need a stop character.
     // Note: currently assumes totally correct input.
-    chemFormula = chemFormula + '@'
-    
+    chemFormula = chemFormula + '@'    
+
+    atoms = new Map();
     CurrChar = chemFormula.slice(0);
 
     for (let i = 1; i < chemFormula.length; i++) {
@@ -38,6 +42,7 @@ molFormula = function( chemFormula ) {
             } else {
                 //Number finished, chem symbol starts
                 atoms.set( currChemSymb, Number( currNumString ) );
+                molFormula = mergeFormulae ( molFormula, atoms );
                 currChemSymb = nextChar;
             }
         } else if ( nextChar !== "@" ) {
@@ -47,7 +52,8 @@ molFormula = function( chemFormula ) {
                 currNumString = nextChar;
             } else if ( isUppercase( nextChar ) ) {
                 //End of a chem symbol for which the number of atoms must be 1
-                molFormula.set( currSymb, number(1) )
+                atoms.set( currSymb, number(1) )
+                molFormula = mergeFormulae ( molFormula, atoms );
                 //Start a new symbol
                 currChemSymb = nextChar;    
             } else {
@@ -55,11 +61,33 @@ molFormula = function( chemFormula ) {
                 currSymb += nextChar;
             }
         }
+        atoms.clear();
         currChar = nextChar;
     }
 }
 
-    // Set communication output element
-    var outputPara = document.getElementById( 'output');
-    outputPara.textContent = '';
+function makeOutputString( molFormula ) {
+    var outString;
+    for (let [key, value] of molFormula.entries()) {
+        outString += key + '<span>' + value + '</span>';
+    }
+}
+
+currMolFormula = new Map();
+var currOutputString;
+var inputForm = document.getElementById( 'input-form' );
+
+// Form submission listener
+inputForm.addEventListener( 'submit', function ( event ) {
+    event.preventDefault();
+    // Get the input
+    var currChemFormula = document.getElementById( 'chem-formula-input' ).value;
+} );
+
+currMolFormula = makeMolFormula( currChemFormula );
+currOutputString = makeOutputString ( currMolFormula );
+
+// Set communication output element
+var outputPara = document.getElementById( 'output');
+outputPara.innerHTML = currOutputString;
 
